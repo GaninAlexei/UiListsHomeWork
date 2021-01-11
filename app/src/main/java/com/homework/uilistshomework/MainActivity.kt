978b6b54ac08b072c1e7b37be599c31bb15c0a93
Anna_Anfilova <Anna_Anfilova@x5.ru>
2021-01-11 20:30:24 +0300

    list adapter

--- app/src/main/java/com/homework/uilistshomework/MainActivity.kt
+++ app/src/main/java/com/homework/uilistshomework/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.homework.uilistshomework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
 import com.homework.uilistshomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MoviesAdapter.IMovieClick {
+
+    private lateinit var adapter: MoviesAdapter
 
     @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,38 +16,31 @@ class MainActivity : AppCompatActivity(), MoviesAdapter.IMovieClick {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = MoviesAdapter()
+        adapter = MoviesAdapter(this)
         binding.recyclerView1.adapter = adapter
-        adapter.list = MockUtil.getHeaderMoviesList(this)
+        adapter.submitList(MockUtil.getHeaderMoviesList(this))
 
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
 
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                if (tab == null) return
-
+            override fun onTabSelected(tab: TabLayout.Tab) {
                 if (tab.position == 0) {
-
-                    binding.recyclerView1.adapter = adapter
-                    adapter.list = MockUtil.getHeaderMoviesList(this@MainActivity)
-
+                    adapter.submitList(MockUtil.getHeaderMoviesList(this@MainActivity))
                 } else {
-
-                    binding.recyclerView1.adapter = adapter
-                    adapter.list = MockUtil.getHeaderWatchedMoviesList(this@MainActivity)
-
+                    adapter.submitList(MockUtil.getHeaderWatchedMoviesList(this@MainActivity))
                 }
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
 
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
 
         })
 
     }
 
-    override fun onDelete(position: Int) {
-        TODO("Not yet implemented")
+    override fun onDelete(item: Item) {
+        val list = adapter.currentList.toMutableList().also { it.remove(item) }
+        adapter.submitList(list)
     }
 
 }
--- app/src/main/java/com/homework/uilistshomework/MoviesAdapter.kt
+++ app/src/main/java/com/homework/uilistshomework/MoviesAdapter.kt
@@ -5,22 +5,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.homework.uilistshomework.databinding.CardViewBinding
 import com.homework.uilistshomework.databinding.ItemHeaderBinding
 import com.homework.uilistshomework.databinding.ItemMovieBinding
 
 
-class MoviesAdapter: ListAdapter<Item.WatchedMovie, BaseViewHolder>(MoviesDiffCallback()) {
-
-    var list: List<Item> = emptyList()
-        set(value){
-            field = value
-            notifyDataSetChanged()
-        }
+class MoviesAdapter(private val listener: IMovieClick) : ListAdapter<Item, BaseViewHolder>(MoviesDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-        return when(viewType){
+        return when (viewType) {
             MOVIE_ITEM_TYPE -> MovieViewHolder(parent)
             HEADER_ITEM_TYPE -> HeaderViewHolder(parent)
             else -> WatchedMoviesViewHolder(parent)
@@ -28,7 +21,7 @@ class MoviesAdapter: ListAdapter<Item.WatchedMovie, BaseViewHolder>(MoviesDiffCa
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-        when(val item = list[position]){
+        when (val item = getItem(position)) {
             is Item.Movie -> (holder as MovieViewHolder).bind(item)
             is Item.Header -> (holder as HeaderViewHolder).bind(item)
             is Item.WatchedMovie -> (holder as WatchedMoviesViewHolder).bind(item)
@@ -37,79 +30,77 @@ class MoviesAdapter: ListAdapter<Item.WatchedMovie, BaseViewHolder>(MoviesDiffCa
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return currentList.size
     }
 
     override fun getItemViewType(position: Int): Int {
-        return when (list[position]){
+        return when (getItem(position)) {
             is Item.Movie -> MOVIE_ITEM_TYPE
             is Item.Header -> HEADER_ITEM_TYPE
             is Item.WatchedMovie -> WATCHED_ITEM_TYPE
         }
     }
 
-    companion object{
+    companion object {
         const val MOVIE_ITEM_TYPE = 1
         const val HEADER_ITEM_TYPE = 2
         const val WATCHED_ITEM_TYPE = 3
     }
 
-    class MovieViewHolder(private val binding: ItemMovieBinding): BaseViewHolder(binding.root){
+    class MovieViewHolder(private val binding: ItemMovieBinding) : BaseViewHolder(binding.root) {
 
-        constructor(parent: ViewGroup): this(
-            ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        constructor(parent: ViewGroup) : this(
+                ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
 
         fun bind(item: Item.Movie) {
-            val binding = ItemMovieBinding.bind(itemView)
             binding.textCountryName.text = item.textMovieName
         }
     }
 
-    class HeaderViewHolder(private val binding: ItemHeaderBinding): BaseViewHolder(binding.root){
-        constructor(parent: ViewGroup): this(
-            ItemHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    class HeaderViewHolder(private val binding: ItemHeaderBinding) : BaseViewHolder(binding.root) {
+        constructor(parent: ViewGroup) : this(
+                ItemHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
 
-        fun bind(item: Item.Header){
+        fun bind(item: Item.Header) {
             binding.root.text = item.text
         }
     }
 
-    class WatchedMoviesViewHolder(private val binding: CardViewBinding): BaseViewHolder(binding.root){
+    inner class WatchedMoviesViewHolder(private val binding: CardViewBinding) : BaseViewHolder(binding.root) {
 
-        constructor(parent: ViewGroup): this(
-            CardViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        constructor(parent: ViewGroup) : this(
+                CardViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
 
-        fun bind(item: Item.WatchedMovie){
-            val binding = CardViewBinding.bind(itemView)
+        fun bind(item: Item.WatchedMovie) {
             binding.textTitle.text = item.title
             binding.textDescription.text = item.description
             binding.img.setImageResource(item.image)
 
-            binding.basket.setOnClickListener(object: View.OnClickListener {
-                override fun onClick(v: View?) {
-
-                }
-
-            })
+            binding.basket.setOnClickListener { listener.onDelete(item) }
 
         }
     }
 
-    interface IMovieClick{
-        fun onDelete(position: Int)
+    interface IMovieClick {
+        fun onDelete(position: Item)
     }
 
 }
 
-class MoviesDiffCallback: DiffUtil.ItemCallback<Item.WatchedMovie>(){
-    override fun areItemsTheSame(oldItem: Item.WatchedMovie, newItem: Item.WatchedMovie): Boolean {
-        return oldItem.title == newItem.title
+object MoviesDiffCallback : DiffUtil.ItemCallback<Item>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+        when {
+            oldItem is Item.WatchedMovie && newItem is Item.WatchedMovie -> {
+                return oldItem.title == newItem.title
+            }
+            else -> return true
+        }
     }
 
-    override fun areContentsTheSame(oldItem: Item.WatchedMovie, newItem: Item.WatchedMovie): Boolean {
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
         return areItemsTheSame(oldItem, newItem)
     }
 
